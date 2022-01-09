40fcfb3458b4322479b839c1fc7141a7cc75dcd6
Gabriel Lira <devzando8182@gmail.com>
2022-01-09 18:08:35 -0300

    horizontal mode 90%

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,39 +1,48 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { MdOutlineCalendarViewDay, MdNavigateBefore, MdNavigateNext } from "react-icons/md";
 
 import './styles.css'
 
-import imgItem from '../../assets/imgItem.svg'
-import imgBackground from '../../assets/legend.png'
-import testeimg from '../../assets/solo leveling definitivo 1.svg'
+import { api } from '../../service/api'
+
 
 export default function Home() {
-    return (
-        <div className='container'>
 
+    const divRef = useRef(null)
+    const [data, setData] = useState([])
+
+    async function handlechange() {
+        const { data } = await api.get('/assets/itens.json')
+        setData(data)
+    }
+
+    useEffect(() => {
+        handlechange()
+    },[])
+
+    return (
+        <div className='container' ref={divRef} >
             <header className='container-logo'>
                 <button className='button-logo'>
                     <MdOutlineCalendarViewDay className='logo' />
                 </button>
             </header>
-            <div className='container-itens'>
-
-                <div className='item'>
-                    <img className='img-item' src={imgItem} alt='img-item' />
-                </div>
-                <div className='item'>
-                    <img className='img-item' src={testeimg} alt='img-item' />
-                </div>
-                <div className='item'>
-                    <img className='img-item' src={imgItem} alt='img-item' />
-                </div>
-            </div>
+            {data.map((item, index) => {
+                return (
+                    <div className='container-itens' style={{ backgroundImage: `url(${item.backgroundImgUrl})`}} key={item.id}>
+                        <div className='item'>
+                            <img className='img-item' src={item.imgItem} alt='img-item' />
+                            <span className='text-item'> {item.name} </span>
+                        </div>
+                    </div>
+                )
+            })}
             <div className='container-buttons'>
                 <button className='button'>
-                    <MdNavigateBefore className='navigateitens' size={150} color='#323232'/>
+                    <MdNavigateBefore className='navigateitens' size={150} color='#323232' />
                 </button>
                 <button className='button'>
-                    <MdNavigateNext className='navigateitens' size={150} color='#323232'/>
+                    <MdNavigateNext className='navigateitens' size={150} color='#323232' />
                 </button>
             </div>
         </div>
